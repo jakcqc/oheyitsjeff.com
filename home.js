86666e6dd4f0dd47f36963a44406d390e990c9b3
Jeffreykerley <jakerley180@gmail.com>
2021-09-02 15:54:05 -0500

    assets

--- home.js
+++ home.js
@@ -1,10 +1,16 @@
 let width;
 let height;
 let svg;
-
+let radius;
     document.addEventListener("DOMContentLoaded", function(){
         width = window.innerWidth;
         height = window.innerHeight;
+        if(width < 600){
+          radius = 70;
+        }else{
+          radius = 130;
+        }
+        
         svg = d3.select("#vis").append("svg")
             .attr("width", Math.ceil(width/10)*10)
             .attr("height", Math.ceil(height/10)*10);
@@ -14,76 +20,119 @@ let svg;
     window.location.href = page;
   }
   function createLotus(svg){
+    
     const z = 50,
-          radius = width/15,
-          ogLineSize = "1",
+          ogLineSize = "2px",
           scaleSizeChange = 1;
     let hue = 0,
         hueBody = 0,
-        vhTen = height*.05,
-        cordXY = [45,52,47,35],
-        signCheck = [1,1,1,1],
         newString,
         lineSet = 0,
         angle = 0,
         points = [],
         counter = 1,
-        opacP = .2;
+        opacP = .2,
+        countCirc = 0,
+        numbCirc = 0;
       
-          svg.selectAll(".sphere")
-          .data(d3.range(1))
-          .enter().append("polyline")
-          .style("stroke", "white")
-          .style("stroke-width", ogLineSize)
-          .attr("points", getCirclePoints) 
-          .attr("fill","black")
-          .attr("class","sphere")
-          .attr("stroke-linecap", "round");
-          // .on("mouseover", mouseover);
-          
-          // svg.selectAll(".cum")
-          // .data(d3.range(1))
-          // .enter().append("polyline")
-          // .style("stroke", "white")
-          // .style("stroke-width", ogLineSize)
-          // .attr("points", points) 
-          // .attr("fill","black")
-          // .attr("class","cum")
-          // .attr("stroke-linecap", "round");
+        var filter = svg.append("defs")
+          .append("filter")
+            .attr("id", "blur")
+          .append("feGaussianBlur")
+            .attr("stdDeviation", 1);
+            svg.selectAll(".sphere")
+            .data(d3.range(1))
+            .enter().append("polyline")
+            .style("stroke", "rgb(25,210,42)")
+            .style("stroke-width", ogLineSize)
+            .attr("points", getCirclePoints) 
+            .attr("fill","rgb(0,0,0,.7)")
+            .attr("class","sphere")
+            .attr("filter", "url(#blur)")
 
-          // .on("mouseover", mouseover);
-          // svg.selectAll(".sphere")
-          //   .data(d3.range(1))
-          //   .attr("points", tester)
-    function tester(){
+            .style("stroke-linejoin", "round");  // shape the line join
+            let fart = 
+              svg.selectAll(".sphere")
+                        .data(d3.range(14))
+                        .enter().append("polyline")
+                        .style("stroke", "rgb(25,210,42)")
+                        .style("stroke-width", ogLineSize)
+                        .style("stroke-linejoin", "round")
+                        .attr("points", getCirclePoints) 
+                        .attr("fill","rgb(0,0,0,.15)")
+                        .attr("filter", "url(#blur)")
+
+                        .attr("class","sphere");
+                  // shape the line join
+              
+                  svg.selectAll(".cum")
+                  .data(d3.range(20))
+                  .enter().append("polyline")
+                  .style("stroke", "black")
+                  .style("stroke-width", "2px")
+                  .attr("points", getCirclePoints) 
+                  .attr("fill","none")
+                  .attr("class","cum")
+                  .attr("filter", "url(#blur)")
+                  .style("stroke-linejoin", "round");  // shape the line join
+                  const allLoad = svg.selectAll('polyline');
+                  console.log(allLoad);
+
+                  let i = 0;
+                  const endLoad = setInterval(function(){
+                   // console.log(i);
+                    if(i > allLoad.length){
+                      clearInterval(endLoad);
+                    }
+                    svg.selectAll('polyline')[i]
+                      .transition()
+                      .duration(500)
+                        .style("stroke", "rgb(25,210,42)")
+  
+                    i++;
+                  },1000);
+                  
+                  
+                
+    function ran(max) {
+      return Math.floor(Math.random() * max) + 5;
+    }   
+    function tester(points){
       let count = 0;
-      console.log('before points',points);
       while(count < points.length){
-        if(count%5 == 0){
+        if(count%4 == 0){
+          points[count] = points[count] - 6;
+        }
+        else if(count%5){
           points[count] = points[count] -3;
+
         }else{
           points[count] = points[count] + 2;
         }
         count++;
       }
-      console.log('points after',points);
       return points;
       
     } 
     function getCirclePoints(){
       let x = 0;
       let y = 0;
-      console.log(angle);
-      while(angle <= 6.4){
+      angle = 0;
+      let currentPoints = [];
+      console.log("circ", countCirc);
+      radius = radius + (countCirc);
+      while(angle <= 6.25){
         x = (radius * Math.sin(angle)) + width/5;
-        y = (radius * Math.cos(angle)) + height/4;
-        points.push(x);
-        points.push(y);
-        console.log(x,y);
-        angle = angle + .1;
+        y = (radius * Math.cos(angle)) + height/6;
+        currentPoints.push(x);
+        currentPoints.push(y);
+        angle = angle + .05;
       }
-      points = points.map(x => x * 2.5);
-    return points;
+      currentPoints.push(currentPoints[0]);
+      currentPoints.push(currentPoints[1]);
+      
+      countCirc = countCirc + .75;
+      return tester(currentPoints.map(zz => zz * 2.5));
     }
     function mouseover(i) {
         this.parentNode.appendChild(this);
