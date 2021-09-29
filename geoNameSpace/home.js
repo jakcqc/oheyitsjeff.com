d5746f8f171331d144720e184d96b35e33f79597
Jeffrey Kerley <jeffreykerley@Jeffreys-MacBook-Pro.local>
2021-09-29 18:30:57 -0500

    new type

--- geoNameSpace/home.js
+++ geoNameSpace/home.js
@@ -4,20 +4,69 @@ let svg;
 let radius;
     document.addEventListener("DOMContentLoaded", function(){
         width = window.innerWidth;
+
         height = window.innerHeight;
-        if(width < 600){
-          radius = 70;
-        }else{
-          radius = 130;
-        }
+        console.log(width,height);
         
-        svg = d3.select("#vis").append("svg")
-            .attr("width", Math.ceil(width/10)*10)
-            .attr("height", Math.ceil(height/10)*10);
-          createLotus(svg);
     });
   function goTo(page){
     window.location.href = page;
+  }
+  function setup() {
+    createCanvas(width, height);
+  }
+  
+  function draw() {
+    //background(102);
+    
+    if (mouseIsPressed) {
+      background(0);
+    } else {
+      background(255);
+    }
+    let currentX = .06;
+    let currentY = .5;
+    let currentP = 2;
+    let currentR = 30;
+    for(let x = 0; x < 20; x++){
+      push();
+      translate(width * currentX, height * currentY);
+      rotate(frameCount / -200.0);
+      polygon(0, 0, currentR, currentP);
+      pop();
+      currentP = currentP + 1;
+      // currentR = currentR + 5;
+      currentX = currentX + .05;
+
+    }
+    push();
+    translate(width * 0.0, height * 0.5);
+    //rotate(frameCount / 200.0);
+    polygon(0, 0, 30, 1);
+    pop();
+  
+    // push();
+    // translate(width * 0.5, height * 0.5);
+    // rotate(frameCount / 50.0);
+    // polygon(0, 0, 80, 20);
+    // pop();
+  
+    // push();
+    // translate(width * 0.8, height * 0.5);
+    // rotate(frameCount / -100.0);
+    // polygon(0, 0, 70, 7);
+    // pop();
+  }
+  
+  function polygon(x, y, radius, npoints) {
+    let angle = TWO_PI / npoints;
+    beginShape();
+    for (let a = 0; a < TWO_PI; a += angle) {
+      let sx = x + cos(a) * radius;
+      let sy = y + sin(a) * radius;
+      vertex(sx, sy);
+    }
+    endShape(CLOSE);
   }
   
   
